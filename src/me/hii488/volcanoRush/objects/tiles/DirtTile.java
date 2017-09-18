b47f6693c631bb214ab0dab2f3ee4e0344ed7056
Laurence <hii488@btinternet.com>
2017-09-18 23:25:07 +0100

    Added Damaged overlay
    Overlays "damage" when the things have been partially dug away from.

--- src/me/hii488/volcanoRush/objects/tiles/DirtTile.java
+++ src/me/hii488/volcanoRush/objects/tiles/DirtTile.java
@@ -1,6 +1,13 @@
 package me.hii488.volcanoRush.objects.tiles;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import me.hii488.controllers.GameController;
+import me.hii488.graphics.Camera;
 import me.hii488.handlers.ContainerHandler;
+import me.hii488.handlers.TextureHandler;
 import me.hii488.misc.Settings;
 import me.hii488.registries.EntityRegistry;
 import me.hii488.volcanoRush.objects.OreType;
@@ -9,11 +16,13 @@ import me.hii488.volcanoRush.objects.entities.FallingDirt;
 public class DirtTile extends MineralTile{
 	
 	public int damageValue = 0;
+	public BufferedImage[] overlay;
 	
 	public DirtTile(){super();}
 	public DirtTile(DirtTile t){
 		super(t);
 		this.damageValue = t.damageValue;
+		this.overlay = t.overlay;
 	}
 	
 	@Override
@@ -22,6 +31,11 @@ public class DirtTile extends MineralTile{
 		this.identifier = "dirtTile";
 		this.textureName = "dirtTile_" + this.oreType + ".png";
 		this.isCollidable = true;
+		
+		overlay = new BufferedImage[3];
+		for(int i = 0; i < overlay.length; i++)
+			overlay[i] = TextureHandler.loadTexture("textures/tiles/", "dirtOverlay_" + i + ".png", this);
+
 	}
 	
 	@Override
@@ -59,6 +73,25 @@ public class DirtTile extends MineralTile{
 		this.identifier = "dirtTile" + this.oreType;
 		this.textureName = "dirtTile_" + oreType + ".png";
 		this.setupTextures();
+	}
+	
+	public void render(Graphics g){
+		
+		renderPosA.setX(gridPosition.getAbsX() * Camera.scale * Settings.Texture.tileSize - Camera.cameraPosition.getAbsX());
+		renderPosA.setY(gridPosition.getAbsY() * Camera.scale * Settings.Texture.tileSize - Camera.cameraPosition.getAbsY());
+		renderPosB.setX(renderPosA.getAbsX() + (Settings.Texture.tileSize * Camera.scale));
+		renderPosB.setY(renderPosA.getAbsY() + (Settings.Texture.tileSize * Camera.scale));
+		
+		if(renderPosA.getX() < GameController.windows[0].width && renderPosB.getX() > 0){
+			if(renderPosA.getY() < GameController.windows[0].height && renderPosB.getY() > 0){
+				g.drawImage(currentTexture, renderPosA.getX(), renderPosA.getY(), null);
+				if(damageValue > 0 && damageValue < 4) g.drawImage(overlay[damageValue-1], renderPosA.getX(), renderPosA.getY(), null);
+				if(Settings.Logging.debug && isCollidable){
+					g.setColor(Color.red);
+					g.drawRect(renderPosA.getX(), renderPosA.getY(), (int)(Settings.Texture.tileSize * Camera.scale), (int)(Settings.Texture.tileSize * Camera.scale));
+				}
+			}
+		}
 	}
 	
 	@Override
